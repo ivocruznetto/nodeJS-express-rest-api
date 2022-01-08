b4c189351348f65aee19fcb5edce0f6c53c58dc4
Ivo <ivo.neto@mais.com.br>
2022-01-08 18:11:41 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
+
+conexao.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('conectado com sucesso')
+
+        Tabelas.init(conexao);
+        const app = customExpress();
+        
+        app.listen(3000, () => console.log('Server started on port 3000'));
+    }
+});
